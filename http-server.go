3633cbee1cd0cc0645d5237bcc5a527090c34a0a
yykhomenko <yykhomenko@gmail.com>
2020-11-23 19:44:53 +0200

    added comment

--- http-server.go
+++ http-server.go
@@ -7,6 +7,7 @@ import (
 )
 
 // hey -n 400000 -c 50 http://localhost:8081/
+// wrk -t3 -c50 -d10s --latency http://127.0.0.1:8081/
 func main() {
 	log.Print("http-server listening...")
 	log.Fatal(fasthttp.ListenAndServe(":8081", requestHandler))
